53ce7c23ea1b0cfc5debb66117cc58596642b101
Periklis <perik76@gmail.com>
2024-06-10 22:57:24 +0300

    Tests and code for Database

---
+++ spec/index.spec.js
@@ -0,0 +1,102 @@
+import Database from "../src/index.js"
+import { v4 as uuidv4 } from "uuid"
+
+describe("Database", () => {
+	let db
+
+	beforeEach(() => {
+		db = new Database()
+	})
+
+	// Test Validity
+
+	it("should be an instance of Databace", () => {
+		expect(db).toBeInstanceOf(Database)
+	})
+
+	it("should start with data as an empty array", () => {
+		expect(db.data.length).toBe(0)
+	})
+
+	//Test addData
+
+	it("should be able to add one or more entries with different auto-generated ids", () => {
+		db.addData({ username: "JohnyBeGood" })
+		expect(db.data.length).toBe(1)
+		db.addData({ username: "MaryBeBad" })
+		expect(db.data.length).toBe(2)
+		expect(db.data[0]).toEqual({
+			username: "JohnyBeGood",
+			id: "cb0cad71-3a90-46dc-bb5f-f74518c9457a",
+		})
+		expect(db.data[1]).toEqual({
+			username: "MaryBeBad",
+			id: "c8063fae-5bed-4cc8-84ca-7a995b63fd27",
+		})
+	})
+
+	it("should throw an error if wrong type of data are provided to addData", () => {
+		expect(() => db.addData("Johny")).toThrowError(
+			"The data provided must be an object"
+		)
+	})
+
+	//Test removeData
+
+	it("should be able to find and remove an existing entry, using a provided id", () => {
+		db.addData({ username: "JohnyBeGood" })
+		db.addData({ username: "MaryBeBad" })
+		db.addData({ username: "ABlokeInThePub" })
+		expect(db.data.length).toBe(3)
+		expect(db.data[2].username).toBe("ABlokeInThePub")
+
+		db.removeData("4671db17-3c2e-4cdd-8e61-357bc767610b")
+		expect(db.data.length).toBe(2)
+	})
+
+	it("should throw an error if wrong or no id is provided to removeData", () => {
+		db.addData({ username: "JohnyBeGood" })
+		db.addData({ username: "MaryBeBad" })
+		db.addData({ username: "ABlokeInThePub" })
+		expect(db.data.length).toBe(3)
+
+		expect(() => db.removeData("46b")).toThrowError(
+			"No data with id = 46b were found in the database"
+		)
+		expect(() => db.removeData()).toThrowError(
+			"An ID must be provided"
+		)
+	})
+
+	//Test updateData
+
+	it("should be able to update an entry by id with the provided data", () => {
+		db.addData({ username: "JohnyBeGood" })
+		db.addData({ username: "MaryBeBad" })
+		db.addData({ username: "ABlokeInThePub" })
+		expect(db.data[2].username).toBe("ABlokeInThePub")
+
+		db.updateData("4671db17-3c2e-4cdd-8e61-357bc767610b", {
+			username: "ADifferentBlokeInThePub",
+		})
+		expect(db.data[2].username).toBe("ADifferentBlokeInThePub")
+	})
+
+	it("should throw an error if wrong or no id is provided to updateData", () => {
+		db.addData({ username: "JohnyBeGood" })
+		db.addData({ username: "MaryBeBad" })
+		db.addData({ username: "ABlokeInThePub" })
+		expect(db.data[2].username).toBe("ABlokeInThePub")
+
+		expect(() =>
+			updateData("46b", {
+				userName: "AnotherBlokeInThePub",
+			}).toThrowError(
+				"No data with id = 46b were found in the database"
+			)
+		)
+		expect(() => db.updateData()).toThrowError(
+			"An ID of the entry to be updated, must be provided"
+		)
+	})
+})
---
+++ src/index.js
@@ -0,0 +1,90 @@
+import { v4 as uuidv4 } from "uuid"
+
+const ids = new Map()
+ids.set(1, "cb0cad71-3a90-46dc-bb5f-f74518c9457a")
+ids.set(2, "c8063fae-5bed-4cc8-84ca-7a995b63fd27")
+ids.set(3, "4671db17-3c2e-4cdd-8e61-357bc767610b")
+ids.set(4, "bcd8c6e9-80f8-41d4-9a04-df718d537085")
+ids.set(5, "f7e4f895-9c90-4e99-9104-1845661994d7")
+ids.set(6, "b1ce3bbd-0ef8-449c-8b5c-2f870645f61d")
+ids.set(7, "18061dc0-8e92-4b12-8ac6-7d1627b991da")
+ids.set(8, "6348bc42-0e62-47bd-9828-ad5f2e3ddcd0")
+ids.set(9, "5cc95eea-c597-4e33-84a6-8406d5a599af")
+ids.set(10, "2bc1042d-6731-4c6a-b08f-f4224943873e")
+
+class Database {
+	#data
+
+	constructor(idGenerator = uuidv4) {
+		this.idGenerator = uuidv4
+		this.#data = []
+	}
+
+	get data() {
+		return [...this.#data]
+	}
+
+	createIds() {
+		const nextId = this.#data.length + 1
+		return ids.get(nextId)
+	}
+
+	addData(data) {
+		if (!data || typeof data !== "object") {
+			throw new Error("The data provided must be an object")
+		} else {
+			// const newData = { id: this.idGenerator(), ...data }
+			data.id = this.createIds()
+			const newData = { ...data }
+			this.#data.push(newData)
+		}
+	}
+
+	findDataById(id) {
+		const idIndex = this.#data.findIndex((idi) => idi.id === id)
+		if (idIndex === -1) {
+			return false
+		} else {
+			return [idIndex, this.#data[idIndex]]
+		}
+	}
+
+	removeData(id) {
+		if (!id) {
+			throw new Error("An ID must be provided")
+		}
+		const entryToRemove = this.findDataById(id)[1]
+		if (!entryToRemove) {
+			throw new Error(
+				`No data with id = ${id} were found in the database`
+			)
+		} else {
+			this.#data = this.#data.filter((dt) => dt.id !== id)
+		}
+	}
+
+	updateData(id, newData) {
+		if (!id) {
+			throw new Error(
+				"An ID of the entry to be updated, must be provided"
+			)
+		}
+		if (typeof newData !== "object") {
+			throw new Error(
+				`New data must be an object with a numerical ID`
+			)
+		}
+		const index = this.findDataById(id)[0]
+		if (index) {
+			this.#data[index] = { ...this.#data[index], ...newData }
+		} else {
+			throw new Error(
+				`No data with id = ${id} were found in the database`
+			)
+		}
+	}
+}
+
+export default Database
+
+
