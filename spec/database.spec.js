3cc2c46c28150734bed2bacfc2f77db51269f178
Will Baxter <bax.will@googlemail.com>
2024-06-10 15:12:24 +0100

    post database should have same getdata method as database

--- spec/database.spec.js
+++ spec/database.spec.js
@@ -1,72 +1,130 @@
 import { users, posts } from "../src/dummydata.js";
 import Database from "../src/Database.js";
 import UserDatabase from "../src/UserDatabase.js";
+import PostDatabase from "../src/PostDatabase.js";
 
 describe("Database", () => {
-    let myDatabase
+  let myDatabase;
 
-    beforeEach(() => {
-        myDatabase = new Database([...users])
-    })
-
+  beforeEach(() => {
+    myDatabase = new Database([...users]);
+  });
 
   it("should construct with data and return the data with a getData method", () => {
-    expect(myDatabase.getData()).toEqual(users)
+    expect(myDatabase.getData()).toEqual(users);
   });
 
   it("should find a data object by its ID", () => {
-    expect(myDatabase.findById(5)).toEqual({ id: 5, username: "bob_builder", email: 'bobbuilder@example.com' })
-  })
+    expect(myDatabase.findById(5)).toEqual({
+      id: 5,
+      username: "bob_builder",
+      email: "bobbuilder@example.com",
+    });
+  });
 
   it("should update data after finding it by ID", () => {
-    myDatabase.updateById(1, 'username', 'Mr_Pistachio')
+    myDatabase.updateById(1, "username", "Mr_Pistachio");
 
-    expect(myDatabase.findById(1).username).toEqual('Mr_Pistachio')
-  })
+    expect(myDatabase.findById(1).username).toEqual("Mr_Pistachio");
+  });
 
   it("should remove data by ID", () => {
-    myDatabase.removeById(1)
+    myDatabase.removeById(1);
 
-    expect(() => {myDatabase.findById(1)}).toThrowError('No data found with this ID')
-  })
+    expect(() => {
+      myDatabase.findById(1);
+    }).toThrowError("No data found with this ID");
+  });
 });
 
 describe("UserDatabase", () => {
-    let myUserDatabase;
+  let myUserDatabase;
 
+  beforeEach(() => {
+    myUserDatabase = new UserDatabase(new Database([...users]));
+  });
+
+  it("should have the same detData method as injected database", () => {
+    expect(myUserDatabase.getData()).toEqual([...users]);
+  });
+
+  it("should have the same getById method as injected database", () => {
+    expect(myUserDatabase.findById(5)).toEqual({
+      id: 5,
+      username: "bob_builder",
+      email: "bobbuilder@example.com",
+    });
+  });
+
+  it("should have the same update method as injected database", () => {
+    myUserDatabase.updateById(8, "username", "Shane-o");
+
+    expect(myUserDatabase.findById(8).username).toEqual("Shane-o");
+  });
+
+  it("should have the same removeDataById method as injected database", () => {
+    myUserDatabase.removeById(1);
+
+    expect(() => {
+      myUserDatabase.findById(1);
+    }).toThrowError("No data found with this ID");
+  });
+
+  it("should throw an error when trying to update username with a string of less than 5 characters", () => {
+    expect(() => {
+      myUserDatabase.updateById(1, "username", "four");
+    }).toThrowError("Usernames must be unique and over 5 characters");
+  });
+
+  it("should throw an error when trying to update username with a non-unique username", () => {
+    expect(() => {
+      myUserDatabase.updateById(1, "username", "franklin_jr");
+    }).toThrowError("Usernames must be unique and over 5 characters");
+  });
+});
+
+describe("PostrDatabase", () => {
+    let myPostDatabase;
+  
     beforeEach(() => {
-        myUserDatabase = new UserDatabase(new Database([...users]))
-    })
-
-    it('should have the same detData method as injected database', () =>{
-        
-       expect(myUserDatabase.getData()).toEqual([...users])
-    })
-
-    it('should have the same getById method as injected database', () =>{
-        
-        expect(myUserDatabase.findById(5)).toEqual({ id: 5, username: "bob_builder", email: 'bobbuilder@example.com' })
-     })
-
-     it('should have the same update method as injected database', () => {
-        myUserDatabase.updateById(8, 'username', 'Shane-o')
-
-        expect(myUserDatabase.findById(8).username).toEqual('Shane-o')
-     })
-
-     it("should have the same removeDataById method as injected database", () => {
-        myUserDatabase.removeById(1)
-    
-        expect(() => {myUserDatabase.findById(1)}).toThrowError('No data found with this ID')
-      })
-
-      it("should throw an error when trying to update username with a string of less than 5 characters", () => {
+      myPostDatabase = new PostDatabase(new Database([...posts]));
+    });
   
-        expect(() => {myUserDatabase.updateById(1, 'username', 'four')}).toThrowError("Usernames must be unique and over 5 characters")
-      })
-
-      it("should throw an error when trying to update username with a non-unique username", () => {
+    it("should have the same detData method as injected database", () => {
+      expect(myPostDatabase.getData()).toEqual([...posts]);
+    });
   
-        expect(() => {myUserDatabase.updateById(1, 'username', "franklin_jr")}).toThrowError("Usernames must be unique and over 5 characters")
-      })
-})
+    // it("should have the same getById method as injected database", () => {
+    //   expect(myPostDatabase.findById(5)).toEqual({
+    //     id: 5,
+    //     username: "bob_builder",
+    //     email: "bobbuilder@example.com",
+    //   });
+    // });
+  
+    // it("should have the same update method as injected database", () => {
+    //     myPostDatabase.updateById(8, "username", "Shane-o");
+  
+    //   expect(myPostDatabase.findById(8).username).toEqual("Shane-o");
+    // });
+  
+    // it("should have the same removeDataById method as injected database", () => {
+    //   myUserDatabase.removeById(1);
+  
+    //   expect(() => {
+    //     myPostDatabase.findById(1);
+    //   }).toThrowError("No data found with this ID");
+    // });
+  
+    // it("should throw an error when trying to update username with a string of less than 5 characters", () => {
+    //   expect(() => {
+    //     myPostDatabase.updateById(1, "username", "four");
+    //   }).toThrowError("Usernames must be unique and over 5 characters");
+    // });
+  
+    // it("should throw an error when trying to update username with a non-unique username", () => {
+    //   expect(() => {
+    //     myPostDatabase.updateById(1, "username", "franklin_jr");
+    //   }).toThrowError("Usernames must be unique and over 5 characters");
+    // });
+  });
---
+++ src/PostDatabase.js
@@ -0,0 +1,33 @@
+import Database from "./Database.js";
+
+class PostDatabase {
+  #database;
+
+  constructor(database) {
+    this.#database = database;
+  }
+
+  getData() {
+    return this.#database.getData();
+  }
+
+  findById(id) {
+    return this.#database.findById(id);
+  }
+
+  updateById(id, key, value) {
+    if (key === "username") {
+        const foundData = this.getData().find((element) => element.username === value)
+      if (!(value.length > 6) || foundData) {
+          throw new Error("Usernames must be unique and over 5 characters");
+        }
+    }
+    return this.#database.updateById(id, key, value);
+  }
+
+  removeById(id) {
+    return this.#database.removeById(id);
+  }
+}
+
+export default PostDatabase;
