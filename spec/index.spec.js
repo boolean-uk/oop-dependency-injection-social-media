fa76fd1942fb6eb908e700e2fabae58c696dce7f
Farshad <f.bagdeli13@gmail.com>
2024-06-11 10:31:34 +0200

    test for userDatabase Done

--- spec/index.spec.js
+++ spec/index.spec.js
@@ -39,3 +39,35 @@ describe('Database', () => {
     expect(myArray.length).toBe(1)
   })
 })
+
+describe('UserDatabase', ()=> {
+  
+  let myUserDb
+  beforeEach(() => {
+    myUserDb = new UserDatabase
+    myUserDb.add('Farshad')
+    myUserDb.add('Nathan')
+    myUserDb.add('Lighing')
+    myUserDb.add('DarkSoul')
+  })
+
+  it('should be an UserDatabase', () => {
+    expect(myUserDb).toBeInstanceOf(UserDatabase)
+  })
+
+  it('should add a user to database', () => {
+    expect(myUserDb.userDataBase.length).toBe(4)
+  })
+
+  it('should length be 3 after the remove ', () => {
+    myUserDb.removeById(4)
+    expect(myUserDb.userDataBase.length).toBe(3)
+  })
+
+  it('should change DarkSoul to NightKing', () => {
+    myUserDb.updateById(4, 'NightKing')
+    const result = myUserDb.userDataBase
+    expect(result[3].name).toBe('NightKing')
+  })
+
+})
