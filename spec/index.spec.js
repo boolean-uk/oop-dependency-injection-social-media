5549ad27576fb58904e89d8acbd94bf4862a9abb
JOANNABUUMA1 <nabuumaj6@gmail.com>
2024-06-13 02:18:48 +0100

    Core without tests

--- spec/index.spec.js
+++ spec/index.spec.js
@@ -1,7 +1,12 @@
+import Database, { UserDatabase, PostDatabase } from "../src/index.js";
+
 describe("Core Criteria", () => {
 
-    it("should add data",()=>{
-        
-    })
+    it("should add data",()=>{})
 
-})
+    it("should remove data",()=>{})
+
+    it("should search data",()=>{})
+
+    it("should update data",()=>{})
+});
