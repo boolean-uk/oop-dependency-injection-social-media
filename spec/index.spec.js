b575c802b5eeae9fd18e7ed96b3cedbcfa78565a
JOANNABUUMA1 <nabuumaj6@gmail.com>
2024-06-13 02:36:02 +0100

    Core; it should add data

--- spec/index.spec.js
+++ spec/index.spec.js
@@ -1,12 +1,27 @@
 import Database, { UserDatabase, PostDatabase } from "../src/index.js";
 
 describe("Core Criteria", () => {
+        let database;
+        let userDatabase;
+        let postDatabase;
+      
+        beforeEach(() => {
+          database = new Database([]);
+          userDatabase = new UserDatabase(database);
+          postDatabase = new PostDatabase(database);
+        });
+        
+      
+    it("should add data",()=>{
+        expect(database).toBeInstanceOf(Database);
+        expect(userDatabase).toBeInstanceOf(UserDatabase);
+         expect(postDatabase).toBeInstanceOf(PostDatabase);
 
-    it("should add data",()=>{})
+    })
 
-    it("should remove data",()=>{})
+   // it("should remove data",()=>{})
 
-    it("should search data",()=>{})
+    //it("should search data",()=>{})
 
-    it("should update data",()=>{})
+    //it("should update data",()=>{})
 });
--- src/index.js
+++ src/index.js
@@ -1,73 +1,12 @@
 export default class Database {
-    #rows = [];
-    #id;
-    constructor(input = []) {
-      this.#rows = input;
-      this.#id = 1;
-    }
-  
-    add(row) {
-      let insert = { ...row, id: this.#id };
-      this.#rows.push(insert);
-      this.#id++;
-      return this.#rows;
-    }
-  
-    remove(id) {
-      let updatedRows = this.#rows.filter((row) => row.id !== id);
-      this.#rows = updatedRows;
-      return this.#rows;
-    }
-  
-    findById(id) {
-      let found = this.#rows.find((row) => row.id === id);
-      return found;
-    }
-  
-    update(id, data) {
-      let found = this.findById(id);
-      found = { ...found, ...data };
-      return found;
-    }
-  }
-  
-  export class UserDatabase {
-    #users;
-    constructor(database) {
-      this.#users = database;
-    }
-  
-    add(user) {
-      return this.#users.add(user);
-    }
-    remove(id) {
-      return this.#users.remove(id);
-    }
-    findById(id) {
-      return this.#users.findById(id);
-    }
-    update(id, user) {
-      return this.#users.update(id, user);
-    }
-  }
-  
-  export class PostDatabase {
-    #posts;
-    constructor(database) {
-      this.#posts = database;
-    }
-  
-    add(data) {
-      return this.#posts.add(data);
-    }
-    remove(id) {
-      return this.#posts.remove(id);
-    }
-    findById(id) {
-      return this.#posts.findById(id);
-    }
-    update(id, data) {
-      return this.#posts.update(id, data);
-    }
-  }
-  
+
+}
+
+
+export class UserDatabase {
+
+}
+
+export class PostDatabase {
+    
+}
